df709a856a2182f75c8ecb9c69ec28658e95beb9
Pawel <pawel.html@gmail.com>
2017-11-10 13:04:12 +0100

    first commit

---
+++ indexDB.js
@@ -0,0 +1,30 @@
+import idb from "idb";
+
+// add data to indexDB:
+var dbPromise = idb.open("test-db", 1, function(upgradeDb) {
+  var keyValStore = upgradeDb.createObjectStore("keyval");
+  keyValStore.put("world", "hello");
+});
+
+// read data from indexDB, we create transaction:
+dbPromise
+  .then(function(db) {
+    var tx = db.transaction("keyval");
+    var keyValStore = tx.objectStore("keyval");
+    return keyValStore.get("hello");
+  })
+  .then(function(val) {
+    console.log("The value of hello is ", val);
+  });
+
+// read / write, another value to the object store, we create transaction:
+dbPromise
+  .then(function(db) {
+    var tx = db.transaction("keyval", "readwrite");
+    var keyValStore = tx.objectStore("keyval");
+    keyValStore.put("bar", "foo");
+    return tx.complete;
+  })
+  .then(function() {
+    console.log("Addded bar:foo to keyval");
+  });
